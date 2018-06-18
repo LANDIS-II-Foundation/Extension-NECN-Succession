4b8c2d2eb11b6d224d31d709dd0d20e1624ca402
Robert Scheller <rschell@ncsu.edu>
2018-06-18 14:22:50 -0400

    Reorganizing tests.

--- testing/BiomassHarvest_NECN/BatchFile.bat
+++ testing/BiomassHarvest_NECN/BatchFile.bat
@@ -1,9 +1,13 @@
 rem Batch File to Run a Scenario 
+
 rem The 'rem' keyword indicates that this is a remark
+
 
 call landis-ii scenario_landscape.txt
 
+
 rem call landis-ii scenario-single-cell.txt
+
 
 pause
 
