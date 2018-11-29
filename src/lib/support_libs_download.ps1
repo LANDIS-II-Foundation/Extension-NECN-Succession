4dd555b380841977e6543eb173eaf8ab4afe798e
Robert Scheller <rschell@ncsu.edu>
2018-11-29 08:43:31 -0500

    A climate library v4 test dll and installer created.

--- src/lib/support_libs_download.ps1
+++ src/lib/support_libs_download.ps1
@@ -14,6 +14,7 @@ $dlls = "Landis.Library.AgeOnlyCohorts-v3.dll",
 "Landis.Library.Parameters-v2.dll",
 "Landis.Library.Biomass-v2.dll",
 "Landis.Library.Climate-v3.dll",
+"Landis.Library.Climate-v4.dll",
 "Landis.Library.Succession-InputAGB.dll"
 #************************************************
 
