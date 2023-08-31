5bd8968407adec35955bb5bd81a062d186e71027
rmscheller <rmscheller@gmail.com>
2023-08-31 12:13:22 -0400

    Final cleanup before merging into master

--- testing/Core7-NECN6.9-SingleCellCopy/create_normals.bat
+++
@@ -1,8 +0,0 @@
-rem set working_dir = %cd%
-rem TODO change this so that it will automatically pass the current directory to the script
-
-rem print(working_dir)
-
-Rscript --verbose create_normals.R %cd% "./NECN" "./NECN"
-
-pause
