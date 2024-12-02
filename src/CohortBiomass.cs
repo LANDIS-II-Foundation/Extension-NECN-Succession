4bc4d5a7cb5a1a88582b04b7e9ed509fffbd7596
Robert Scheller <rmscheller@gmail.com>
2024-12-02 08:48:09 -0500

    Update CohortBiomass.cs

--- src/CohortBiomass.cs
+++ src/CohortBiomass.cs
@@ -486,8 +486,7 @@ namespace Landis.Extension.Succession.NECN
         /// <summary>
         /// Computes the initial biomass for a cohort at a site.
         /// </summary>
-        public static float[] InitialBiomass(ISpecies species, ISiteCohorts siteCohorts,
-                                            ActiveSite site)
+        public static float[] InitialBiomass(ISpecies species, ActiveSite site)
         {
             IEcoregion ecoregion = PlugIn.ModelCore.Ecoregion[site];
 
